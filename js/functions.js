bbe37064309ffdfcf1ee13dbbb12e43b98b4c084
ValmirBP <valmir.barros57@yahoo.com.br>
2020-11-30 18:36:52 -0300

    email implemaentation

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+/* FAZER A VALIDAÇÃO DE CADA COMPONENTE DO FORMULÁRIO */
+
+
+document.getElementById('fContato').addEventListener('submit', function() {
+    var nome = this.querySelector('input[name=tNome') = nome.value;
+    var tel = this.querySelector('input[name=tPhone') = tel.value;
+    var msg = this.querySelector('input[name=cMessage)') = msg.value;
+});
